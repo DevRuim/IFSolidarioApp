5ba6e428bf9892e225c2976a12ffe24264417e7d
Alon Camilo <camiloalon07@gmail.com>
2026-06-16 15:06:40 -0300

    botao voltar

--- app/src/main/java/com/ifpr/ifsolidarioapp/ui/usuario/HistoricoFragment.kt
+++ app/src/main/java/com/ifpr/ifsolidarioapp/ui/usuario/HistoricoFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -31,6 +34,13 @@ class HistoricoFragment : Fragment() {
                 container,
                 false
             )
+
+        view.findViewById<ImageView>(R.id.btnVoltar)
+            .setOnClickListener {
+
+                findNavController().navigateUp()
+
+            }
 
 
         recycler =
